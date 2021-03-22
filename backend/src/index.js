9b8ea16f5a93d99561c95da5ddd8a860e6ebd9b7
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2021-03-22 13:00:08 +0200

    Up to 3.22

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,69 +15,69 @@ app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 app.get("/info", (req, res, next) => {
-    Person.count({}).then(count => {
-        res.write(`Phonebook has info for ${count} people\n`);
-        res.write((new Date()).toString());
-        res.end();
-    }).catch(e => next(e));
+  Person.count({}).then(count => {
+    res.write(`Phonebook has info for ${count} people\n`);
+    res.write((new Date()).toString());
+    res.end();
+  }).catch(e => next(e));
 });
 
 app.get("/api/persons", (req, res) => {
-    Person.find({}).then(persons => res.json(persons));
+  Person.find({}).then(persons => res.json(persons));
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
-    Person.findById(req.params.id).then(p => res.json(p)).catch(e => next(e));
+  Person.findById(req.params.id).then(p => res.json(p)).catch(e => next(e));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id).then(_ => res.sendStatus(204)).catch(e => next(e));
+  Person.findByIdAndRemove(req.params.id).then(_ => res.sendStatus(204)).catch(e => next(e));
 });
 
 app.post("/api/persons", (req, res, next) => {
-    const {name, number} = req.body;
-    const isValidName = () => name && name.length > 0;
-    const isValidNumber = () => number && number.length > 0;
-    //const isNameAlreadyAdded = () => persons.filter(p => p.name.toLowerCase() === name.toLowerCase()).length > 0;
+  const { name, number } = req.body;
+  const isValidName = () => name && name.length > 0;
+  const isValidNumber = () => number && number.length > 0;
+  // const isNameAlreadyAdded = () => persons.filter(p => p.name.toLowerCase() === name.toLowerCase()).length > 0;
 
-    if(!isValidName()) {
-        return res.status(400).json({ error: "Name must be specified" });
-    } else if(!isValidNumber()) {
-        return res.status(400).json({ error: "Number must be specified" });
-    } else {
-        const newPerson = new Person({
-            name: name,
-            number: number
-        });
-        newPerson.save().then(createdPerson => res.status(201).json(createdPerson)).catch(e => next(e));
-    }
+  if (!isValidName()) {
+    return res.status(400).json({ error: "Name must be specified" });
+  } else if (!isValidNumber()) {
+    return res.status(400).json({ error: "Number must be specified" });
+  } else {
+    const newPerson = new Person({
+      name: name,
+      number: number
+    });
+    newPerson.save().then(createdPerson => res.status(201).json(createdPerson)).catch(e => next(e));
+  }
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
-    const updatedPerson = {
-        name: req.body.name,
-        number: req.body.number
-    };
+  const updatedPerson = {
+    name: req.body.name,
+    number: req.body.number
+  };
 
-    Person.findByIdAndUpdate(req.params.id, updatedPerson, { new: true })
-        .then(person => res.json(person))
-        .catch(e => next(e));
-})
+  Person.findByIdAndUpdate(req.params.id, updatedPerson, { new: true })
+    .then(person => res.json(person))
+    .catch(e => next(e));
+});
 
 app.use((req, res) => {
-    res.status(404).send({ error: "Enpoint not found" });
-})
+  res.status(404).send({ error: "Enpoint not found" });
+});
 
 app.use((error, req, res, next) => {
-    console.error("ERROR", error);
+  console.error("ERROR", error);
 
-    if(error.name === "CastError") {
-        return res.status(400).send({ error: "Invalid id" });
-    } else if(error.name === "ValidationError") {
-        return res.status(400).send({ error: error.message });
-    }
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "Invalid id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: error.message });
+  }
 
-    next(error);
-})
+  next(error);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
--- backend/src/models/person.js
+++ backend/src/models/person.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
-var uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require("mongoose-unique-validator");
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(result => {
-    console.log('connected to MongoDB')
-})
-.catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-});
+  .then(result => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const personSchema = mongoose.Schema({
-    name: { type: String, required: true, unique: true, minLength: 3 },
-    number: { type: String, required: true, minLength: 5 }
+  name: { type: String, required: true, unique: true, minLength: 3 },
+  number: { type: String, required: true, minLength: 5 }
 });
 
 personSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
 });
 
 personSchema.plugin(uniqueValidator);
